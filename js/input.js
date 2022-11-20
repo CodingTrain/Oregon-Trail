308a9ab60904a79fb49748e17f0f379495bccb2d
Daniel Shiffman <daniel.shiffman@gmail.com>
2022-11-20 18:34:53 -0500

    handling other keys

--- js/input.js
+++ js/input.js
@@ -1,3 +1,8 @@
+function other(code) {
+  const codes = [CONTROL, SHIFT];
+  return codes.includes(code);
+}
+
 class Input {
   constructor(x, y) {
     this.buffer = '';
@@ -5,8 +10,10 @@ class Input {
   }
 
   addKey(key, keyCode) {
-    if (keyCode == SHIFT) return;
-    if (keyCode == ENTER) {
+    if (other(keyCode)) return;
+    if (keyCode == BACKSPACE) {
+      this.buffer = this.buffer.substring(0, this.buffer.length - 1);
+    } else if (keyCode == ENTER) {
       console.log('enter');
       const result = this.buffer;
       this.clear();
