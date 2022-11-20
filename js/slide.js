af6a29f2c1105a45f19155a0ed4f0f0f2ae82dc0
Daniel Shiffman <daniel.shiffman@gmail.com>
2022-11-20 10:27:01 -0500

    code formatting

--- js/slide.js
+++ js/slide.js
@@ -1,78 +1,76 @@
 class Slide {
   constructor(json) {
-    this.bar = loadImage("img/bar.png");
-    
+    this.bar = loadImage('img/bar.png');
+
     this.prompt = json.prompt;
     this.choices = json.choices;
-    if (json.img.length != 0)
-      this.img = loadImage(json.img);
+    if (json.img.length != 0) this.img = loadImage(json.img);
     this.title = json.title;
-    
+
     let content = json.content;
     this.font = content.font.name;
     this.fontSize = content.font.size;
     this.textIndent = content.textIndent;
-  
+
     this.renderPosition = createVector();
   }
-  
-  resetRenderPosition(){
+
+  resetRenderPosition() {
     this.renderPosition.set(0, 0);
   }
-  
-  newLine(){
+
+  newLine() {
     this.renderPosition.y += this.fontSize;
   }
-  
-  renderText(indent, str){
-    let x = this.renderPosition.x+(indent+this.textIndent)*this.fontSize;
+
+  renderText(indent, str) {
+    let x = this.renderPosition.x + (indent + this.textIndent) * this.fontSize;
     text(str, x, this.renderPosition.y);
     this.newLine();
   }
-  
-  renderImg(img){
-    let h = (width/img.width)*img.height;
+
+  renderImg(img) {
+    let h = (width / img.width) * img.height;
     image(img, 0, this.renderPosition.y, width, h);
     this.renderPosition.y += h;
   }
-  
-  renderBar(){
+
+  renderBar() {
     this.renderImg(this.bar);
   }
-  
+
   render() {
     textFont(this.font);
     textAlign(LEFT, CENTER);
     textSize(this.fontSize);
     textLeading(this.fontSize);
-    
+
     fill(255);
     noStroke();
-    
+
     imageMode(CORNER);
-    
+
     this.resetRenderPosition();
-    
-    if(this.img){
+
+    if (this.img) {
       this.renderImg(this.img);
-    }else{
+    } else {
       this.renderBar();
       this.newLine();
       this.renderText(0, this.title);
       this.newLine();
     }
-    this.renderText(0, "You may:");
+    this.renderText(0, 'You may:');
     this.newLine();
-    for(let i=0;i<this.choices.length;i++){
+    for (let i = 0; i < this.choices.length; i++) {
       let choice = this.choices[i];
-      if(choice instanceof Array == false){
+      if (choice instanceof Array == false) {
         choice = [choice];
       }
-      for(let c=0;c<choice.length;c++){
+      for (let c = 0; c < choice.length; c++) {
         let str = choice[c];
-        if(c == 0)
-          str = (i+1) + ". " + str;
-        this.renderText((c==0?1:2), str);
+        if (c == 0) str = i + 1 + '. ' + str;
+        this.renderText(c == 0 ? 1 : 2, str);
       }
     }
     this.newLine();
