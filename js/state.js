3b7685578850407b412b2cd90e116a8b62f87773
Kathy <65121394+kfahn22@users.noreply.github.com>
2022-11-21 18:08:30 -0500

    Update state.js
    add occupation and  prices for inventory

--- js/state.js
+++ js/state.js
@@ -1,8 +1,28 @@
 const state = {
   leader: {
     name: "",
+    occupation: null,
     health: 4 // good = 4, adequate = 3, poor = 2, very poor = 1, dead = 0
   },
+  occupation: [{
+      banker: {
+        wealth: 1600,
+        weight: 1
+      }
+    },
+    {
+      carpender: {
+        wealth: 800,
+        weight: 2
+      }
+    },
+    {
+      farmer: {
+        wealth: 400,
+        weight: 3
+      }
+    }
+  ],
   passengers: [{
       name: "",
       health: 4
@@ -26,13 +46,34 @@ const state = {
   ],
   money: 0,
   inventory: {
-    oxen: 0,
-    clothing: 0,
-    food: 0,
-    ammunition: 0,
-    wheels: 0,
-    axel: 0,
-    tongue: 0
+    oxen: {
+      quantity: 0,
+      price: 20
+    },
+    clothing: {
+      quantity: 0,
+      price: 10
+    },
+    food: {
+      quantity: 0,
+      price: 10
+    },
+    ammunition: {
+      quantity: 0,
+      price: 2
+    },
+    wheels: {
+      quantity: 0,
+      price: 10
+    },
+    axle: {
+      quantity: 0,
+      price: 10
+    },
+    tongue: {
+      quantity: 0,
+      price: 0
+    }
   },
   wagon: {
     location: [{ //if want to track on map
@@ -45,5 +86,6 @@ const state = {
     pace: 2, // (steady 2, slow 1, stopped 0)
   },
   distance: 0, // total distance traveled
-  landmark: 0 // distance to next landmark
+  landmark: 0, // distance to next landmark
+  score: 0 // score depends upon initial occupation, distance traveled, number of passengers surviving?
 };
