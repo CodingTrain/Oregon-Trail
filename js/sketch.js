2020444c1bb813c22fccacb17c182d9584430219
Rodak <rodak.titera@gmail.com>
2022-11-21 19:21:40 +0100

    Game state manager class prototype + modified state JSON a tiny bit

--- js/sketch.js
+++ js/sketch.js
@@ -1,4 +1,6 @@
+
 let data;
+let stateManager;
 
 function preload() {
   data = loadJSON('data/slide_data.json');
@@ -9,9 +11,13 @@ let currentSlide = 0;
 
 function setup() {
   createCanvas(280 * 2, 192 * 2);
+  
+  stateManager = new GameStateManager();
+
   for (let i = 0; i < data.slides.length; i++) {
     slides[i] = new Slide(data.slides[i]);
   }
+
 }
 
 function draw() {
