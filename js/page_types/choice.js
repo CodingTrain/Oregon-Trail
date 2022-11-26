231431f95907833bb24c0f2cba3adb9b90e2de91
Daniel Shiffman <daniel.shiffman@gmail.com>
2022-11-26 10:30:51 -0500

    vscode settings

--- js/page_types/choice.js
+++ js/page_types/choice.js
@@ -1,60 +1,60 @@
 class Choice extends Page {
-    loadData(data) {
-        super.loadData(data);
+  loadData(data) {
+    super.loadData(data);
 
-        const img = data.img;
-        if (img.length != 0) this.img = loadImage(img);
+    const img = data.img;
+    if (img.length != 0) this.img = loadImage(img);
 
-        this.loadImportantVariable(data, "prompt");
-        this.loadImportantVariable(data, "choices");
+    this.loadImportantVariable(data, 'prompt');
+    this.loadImportantVariable(data, 'choices');
+  }
+
+  getAction(input) {
+    const index = input - 1;
+
+    const choice = this.choices[index];
+    if (choice === undefined) return undefined;
+
+    return {
+      action: choice.action,
+      actionIndex: index,
+    };
+  }
+
+  render() {
+    this.renderStart();
+
+    if (this.img) {
+      this.renderImg(this.img);
+      this.renderBar();
+      this.newLine();
+    } else {
+      this.renderBar();
+      this.newLine();
+      this.renderText(0, this.title);
+      this.newLine();
     }
+    this.renderText(0, 'You may:');
+    this.newLine();
+    this.renderListText(1, this.choices);
+    this.newLine();
+    this.renderText(0, this.prompt);
 
-    getAction(input) {
-        const index = input - 1;
+    this.updateCursor();
 
-        const choice = this.choices[index];
-        if (choice === undefined) return undefined;
+    this.newLine();
+    this.renderBar();
+  }
 
-        return {
-            action: choice.action,
-            actionIndex: index,
-        };
-    }
+  changeText(changeData) {
+    const choice = this.choices[changeData.index];
+    if (choice === undefined) return;
+    choice.text = changeData.text;
+  }
 
-    render() {
-        this.renderStart();
-
-        if (this.img) {
-            this.renderImg(this.img);
-            this.renderBar();
-            this.newLine();
-        } else {
-            this.renderBar();
-            this.newLine();
-            this.renderText(0, this.title);
-            this.newLine();
-        }
-        this.renderText(0, "You may:");
-        this.newLine();
-        this.renderListText(1, this.choices);
-        this.newLine();
-        this.renderText(0, this.prompt);
-
-        this.updateCursor();
-
-        this.newLine();
-        this.renderBar();
-    }
-
-    changeText(changeData) {
-        const choice = this.choices[changeData.index];
-        if (choice === undefined) return;
-        choice.text = changeData.text;
-    }
-
-    changeChoice(choiceIndex, newText) {
-        const choice = this.choices[choiceIndex];
-        if (choice === undefined) return;
-        choice.text = newText;
-    }
+  changeChoice(choiceIndex, newText) {
+    const choice = this.choices[choiceIndex];
+    if (choice === undefined) return;
+    choice.text = newText;
+  }
 }
